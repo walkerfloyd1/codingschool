ff504f90dee8990cafb5acf8e032f2ff3db4878a
walkerfloyd1 <walkerfloyd@Walkers-MacBook-Pro.local>
2019-03-07 11:53:38 -0800

    easierassignment

---
+++ Class 3/04-ResetCSS/facebook_files/N6djHVfcdP9.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["W6T\/U"]); }
+
+__d("ReasonReactCompat.bs",["ReasonReact.bs"],(function(a,b,c,d,e,f){"use strict";a=b("ReasonReact.bs").wrapJsForReason;f.wrapReactComponent=a}),null);
---
+++ Class 3/04-ResetCSS/facebook_files/XA8k46QoY32.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["SXvAT"]); }
+
+__d("RemoteAction.bs",["bs_curry"],(function(a,b,c,d,e,f){"use strict";__p&&__p();function g(){return 0}function a(){return[g]}function c(a,b){if(b[0]===g){b[0]=a;return b}}function d(a){a[0]=g;return 0}function e(a,c){return b("bs_curry")._1(a[0],c)}f.create=a;f.subscribe=c;f.unsubscribe=d;f.send=e}),null);
